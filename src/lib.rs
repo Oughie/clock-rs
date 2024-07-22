98c45724d868bf58ed9e5e204b1c72ebb8dab6b8
Oughie <montgalletsabinpantin@gmail.com>
2024-07-22 19:09:28 +0200

    Make config file optional

--- src/lib.rs
+++ src/lib.rs
@@ -15,25 +15,26 @@ use clap::Parser;
 use crate::{cli::Args, config::Config, state::State};
 
 pub fn run() -> Result<(), String> {
-    let file_path = match std::env::var("CONF_PATH") {
-        Ok(path) => Ok(path),
-        Err(VarError::NotUnicode(ref s)) => Err(format!(
-            "environment variable was not valid unicode: {:?}",
-            s
-        )),
-        Err(VarError::NotPresent) => match dirs::config_dir() {
-            Some(dir) => match dir.join("clock-rs/conf.toml").to_str() {
-                Some(path) => Ok(path.to_string()),
-                None => Err("path is not valid unicode".into()),
-            },
-            None => Err("no config directory found".into()),
-        },
-    }?;
-
-    let config_str = fs::read_to_string(file_path).map_err(|err| err.to_string())?;
-
-    let mut config: Config = toml::from_str(&config_str).map_err(|err| err.to_string())?;
     let args = Args::parse();
+    let mut config: Config = Config::default();
+
+    if let Some(file_path) = match std::env::var("CONF_PATH") {
+        Ok(path) => Ok(Some(path)),
+        Err(VarError::NotUnicode(ref s)) => {
+            Err(format!("environment variabl is not valid unicode: {:?}", s))
+        }
+        Err(VarError::NotPresent) => match dirs::config_local_dir() {
+            Some(dir) => match dir.join("clock-rs/conf.toml").to_str() {
+                Some(path) => Ok(Some(path.to_string())),
+                None => unreachable!("configuration path is not valid unicode"),
+            },
+            None => Ok(None),
+        },
+    }? {
+        let config_str = fs::read_to_string(file_path).map_err(|err| err.to_string())?;
+        config = toml::from_str(&config_str).map_err(|err| err.to_string())?;
+    }
+
     args.overwrite(&mut config);
 
     State::new(&config)
